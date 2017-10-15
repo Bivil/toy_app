cd08db2667364b479a3bf2f604e69bb89e4fbd6a
Bivil M Jacob <bivilj@gmail.com>
2017-10-15 23:44:56 +0530

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def hello
+    render html: ("<b>Alphabets : </b>" + ('a'..'z').to_a.shuffle.to_s).html_safe
+  end
 end
