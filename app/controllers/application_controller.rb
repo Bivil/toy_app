bf69a5f02007f49631f7530507087c1ef68d3511
Bivil M Jacob <bivilj@gmail.com>
2017-10-15 23:47:25 +0530

    Minor italics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   def hello
-    render html: ("<b>Alphabets : </b>" + ('a'..'z').to_a.shuffle.to_s).html_safe
+    render html: ("<b><i>Alphabets : </i></b>" + ('a'..'z').to_a.shuffle.to_s).html_safe
   end
 end
